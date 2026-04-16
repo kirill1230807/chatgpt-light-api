0f5873f6f421d37ebd9da89b4a36e1a23a707485
Kyrylo <kberegovoj17@gmail.com>
2026-04-16 16:06:26 +0300

    first commit

---
+++ app/src/main/java/com/example/chatgptlightapi/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.chatgptlightapi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.chatgptlightapi.core.ui.theme.ChatGPTLightAPITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChatGPTLightAPITheme {
+
+            }
+        }
+    }
+}
