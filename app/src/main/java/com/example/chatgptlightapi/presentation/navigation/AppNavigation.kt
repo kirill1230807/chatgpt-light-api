0acd2c59ff53ab4a089fb0cd3e6e1ce620feadc8
Kyrylo <kberegovoj17@gmail.com>
2026-05-08 23:49:26 +0300

    feat: add basic screen

--- app/src/main/java/com/example/chatgptlightapi/presentation/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/chatgptlightapi/presentation/navigation/AppNavigation.kt
@@ -2,12 +2,14 @@ package com.example.chatgptlightapi.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.chatgptlightapi.presentation.screens.chat.MainChatScreen
 import com.example.chatgptlightapi.presentation.screens.chat.MainChatScreenContent
+import com.example.chatgptlightapi.presentation.screens.chat.MainChatViewModel
 
 @Composable
 fun AppNavigation() {
@@ -16,6 +18,8 @@ fun AppNavigation() {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
 
     val currentRoute = navBackStackEntry?.destination?.route
+
+    val mainChatViewModel: MainChatViewModel = viewModel()
 
     NavHost(
         navController = navController,
@@ -26,7 +30,9 @@ fun AppNavigation() {
         }
 
         composable<ChatRoute> {
-            MainChatScreen()
+            MainChatScreen(
+                viewModel = mainChatViewModel
+            )
         }
 
         composable<SettingsRoute> {
